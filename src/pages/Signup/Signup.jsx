f7eece3bf6ffe63cbe13c7ba922970c5ce455393
Huzaifa Afraz <huzaifaafraz90@gmail.com>
2024-02-09 00:53:17 +0500

    signup page under development

---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Input from '../../components/Input/Input'
+export default function Signup() {
+  return (
+    <div>
+      <Input type='Name' placeholder='john' label='Name' name='Name'/>
+      <Input type='Name' placeholder='john' label='Name' name='Name'/>
+      <Input type='Name' placeholder='john' label='Name' name='Name'/>
+    </div>
+  )
+}
